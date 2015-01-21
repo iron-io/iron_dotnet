792e90dc92a28638d79cb0a4a0472e818d81d1e5
Alex Litvak <alex.litvak@iron.io>
2015-01-21 15:19:54 +0600

    Fix retrieving subscribers information issue

---
+++ src/IronSharp.IronMQ/PushInfo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace IronSharp.IronMQ
+{
+    public class PushInfo
+    {
+        [JsonProperty("subscribers", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        private List<Subscriber> _subscribers;
+
+        /// <summary>
+        /// How many times to retry on failure.
+        /// Default is 3.
+        /// Maximum is 100.
+        /// </summary>
+        [JsonProperty("retries", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public int? Retries { get; set; }
+
+        /// <summary>
+        /// Delay between each retry in seconds.
+        /// Default is 60.
+        /// </summary>
+        [JsonProperty("retries_delay", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public int? RetriesDelay { get; set; }
+
+        [JsonIgnore]
+        public List<Subscriber> Subscribers
+        {
+            get { return LazyInitializer.EnsureInitialized(ref _subscribers); }
+            set { _subscribers = value; }
+        }
+    }
+}
