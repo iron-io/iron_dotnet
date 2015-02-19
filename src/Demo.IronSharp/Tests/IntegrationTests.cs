75cea8dc172f30f64d00180faafad5eb7dd02dd7
Alex Litvak <alex.litvak@iron.io>
2015-02-20 02:17:25 +0600

    Add unit tests

--- src/Demo.IronSharp/Tests/IntegrationTests.cs
+++ src/Demo.IronSharp/Tests/IntegrationTests.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text.RegularExpressions;
 using IronSharp.Core;
 using IronSharp.IronMQ;
@@ -11,11 +13,14 @@ namespace Demo.IronSharpConsole.Tests
     {
         private IronMqRestClient ironMq;
 
+        private QueueClient queue;
+
         [SetUp]
         public void Init()
         {
             // Put your ".iron.json" file to home directory, eg. C:\Users\YourUsername
-            ironMq = Client.New();
+            ironMq = Client.New();            
+            queue = ironMq.Queue(GetQueueName());
 
             // Or config the client here:
             // ironMq = Client.New(new IronClientConfig
@@ -29,45 +34,198 @@ namespace Demo.IronSharpConsole.Tests
             // });
         }
 
-        [Test]
-        public void PostMessageTest()
+        [TearDown]
+        public void After()
         {
-            QueueClient q = ironMq.Queue(GetQueueName());
-            string messageId = q.Post("some data");
-            Console.WriteLine("Posted message with id {0}", messageId);
+            queue.Delete();
+        }
+
+        [Test]
+        public void PostMessage()
+        {            
+            string messageId = queue.Post("some data");            
             Assert.IsTrue(Regex.IsMatch(messageId, "^[a-f0-9]{19}$"));
         }
 
         [Test]
         public void ReserveMessage()
-        {
-            var q = ironMq.Queue(GetQueueName());
-            for (int i = 0; i < 3; i++)
-                q.Post(i.ToString());
+        {            
+            queue.Post("one");
 
-            var msg = q.Reserve();
+            var msg = queue.Reserve();
             Assert.AreEqual(19, msg.Id.Length);
             Assert.IsTrue(Regex.IsMatch(msg.ReservationId, "^[a-f0-9]{32}$"));
         }
 
         [Test]
         public void ReserveMessages()
-        {
-            var q = ironMq.Queue(GetQueueName());
-            for (int i = 0; i < 3; i++)
-                q.Post(i.ToString());
+        {            
+            for (int i = 0; i < 2; i++)
+                queue.Post(i.ToString());
 
-            var messages = q.Reserve(wait: 12);
-            foreach (var message in messages.Messages)
+            var messagesContainer = queue.Reserve(3);
+            foreach (var message in messagesContainer.Messages)
             {
                 Assert.AreEqual(19, message.Id.Length);
                 Assert.IsTrue(Regex.IsMatch(message.ReservationId, "^[a-f0-9]{32}$"));
             }
         }
 
+        [Test]
+        public void ClearQueue()
+        {            
+            queue.Post("one");
+            queue.Clear();
+            Assert.AreEqual(queue.Info().Size, 0);
+        }
+
+        [Test]
+        public void DeleteReservedMessage()
+        {
+            queue.Clear();
+            PostTwoMessages();
+            var message = queue.Reserve();
+            message.Delete();
+            Assert.AreEqual(queue.Info().Size, 1);
+        }
+
+        [Test]
+        public void DeleteReservedMessageViaQueue()
+        {
+            queue.Clear();
+            PostTwoMessages();
+            var message = queue.Reserve();
+            queue.DeleteMessage(message.Id, message.ReservationId);
+            Assert.AreEqual(queue.Info().Size, 1);
+        }
+
+        [Test]
+        public void DeleteNotReservedMessage()
+        {
+            queue.Clear();
+            queue.Post("one");
+            var messageId = queue.Post("two");            
+            queue.Delete(messageId);
+            Assert.AreEqual(1, queue.Info().Size);
+        }
+
+        [Test]
+        public void DeleteNotReservedMessagePassingEmptyReservationId()
+        {
+            queue.Clear();
+            queue.Post("one");
+            var messageId = queue.Post("two");
+            queue.DeleteMessage(messageId,"");
+            Assert.AreEqual(1, queue.Info().Size);
+        }
+
+        [Test]
+        public void UpdateQueue()
+        {            
+            var expectedTimeout = 66;
+            var expectedExpiration = 3333;
+            queue.Update(new QueueInfo { MessageTimeout = expectedTimeout, MessageExpiration = expectedExpiration });
+
+            var actualQueueInfo = queue.Info();            
+            Assert.AreEqual(expectedTimeout, actualQueueInfo.MessageTimeout);
+            Assert.AreEqual(expectedExpiration, actualQueueInfo.MessageExpiration);
+        }
+
+        [Test]
+        public void DeleteQueue()
+        {
+            var q = ironMq.Queue(GetQueueName());
+            q.Delete();
+            Assert.AreEqual(null, q.Info());
+        }
+
+        [Test]
+        public void GetMessageById()
+        {
+            queue.Clear();
+            var expectedBody = "testGetById";
+            var messageId = queue.Post(expectedBody);
+            Assert.AreEqual(expectedBody,queue.Get(messageId).Body);
+        }
+
+        [Test]
+        public void PeekMessage()
+        {
+            queue.Clear();
+            var firstMessageId = queue.Post("one");
+            queue.Post("two");
+            var peekedMessage = queue.PeekNext();
+            Assert.AreEqual(firstMessageId, peekedMessage.Id);            
+        }
+
+        [Test]
+        public void TouchMessage()
+        {
+            queue.Post("one");
+            var msg = queue.Reserve();
+            var oldReservationId = msg.ReservationId;
+            msg.Touch();
+            Assert.AreNotEqual(oldReservationId, msg.ReservationId);
+        }
+
+        [Test]
+        public void ReleaseMessage()
+        {
+            queue.Post("one");            
+            var msg = queue.Reserve();            
+            Assert.IsTrue(msg.Release());
+        }
+
+        [Test]
+        public void DeleteMessages()
+        {
+            queue.Clear();
+            PostTwoMessages();
+            var messages = queue.Reserve(2);
+            queue.Delete(messages);
+            Assert.AreEqual(0, queue.Info().Size);
+        }
+
+        [Test]
+        public void TurnPull2PushQueue()
+        {
+            var q = ironMq.Queue(GetQueueName());
+            var pushInfo = GenerateSubscribers(1);
+            q.Update(new QueueInfo { PushType = PushType.Multicast, PushInfo = pushInfo});
+
+            Assert.AreEqual(PushType.Multicast, q.Info().PushType);
+        }
+
+        [Test]
+        public void AddSubscribers()
+        {
+            var q = ironMq.Queue(GetQueueName());
+            var pushInfo = GenerateSubscribers(2);
+            q.Update(new QueueInfo { PushType = PushType.Multicast, PushInfo = pushInfo });
+            var actualPushInfo = q.Info().PushInfo;
+
+            Assert.AreEqual(pushInfo.Subscribers.Count, actualPushInfo.Subscribers.Count);
+            Assert.AreEqual(pushInfo.Subscribers[0].Url, actualPushInfo.Subscribers[0].Url);
+            Assert.AreEqual(pushInfo.Subscribers[1].Url, actualPushInfo.Subscribers[1].Url);
+        }
+
+        private void PostTwoMessages()
+        {
+            queue.Post("one");
+            queue.Post("two");
+        }
+
         private String GetQueueName()
-        {           
-            return String.Format("queue{0}", DateTime.Now.Ticks); ;
+        {
+            return String.Format("queue{0}", DateTime.Now.Ticks);
+        }
+
+        private PushInfo GenerateSubscribers(int count)
+        {
+            var subscribes = new List<Subscriber>();
+            for(int i=0;i<count;i++)
+                subscribes.Add(new Subscriber{Url = String.Format("http://myURL{0}",i)});
+            return new PushInfo {Subscribers = subscribes};
         }
     }
 }
