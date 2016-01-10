eeadf48ef42cbd055f39ac0e4c27b2bf467c4338
Reed Allman <rdallman10@gmail.com>
2016-01-10 02:42:28 -0800

    fix host to v3 one, remove others

--- src/IronSharp.IronMQ/IronMqCloudHosts.cs
+++ src/IronSharp.IronMQ/IronMqCloudHosts.cs
@@ -10,16 +10,6 @@
         /// <summary>
         /// Default
         /// </summary>
-        public const string AWS_US_EAST_HOST = "mq-aws-us-east-1.iron.io";
-
-        /// <summary>
-        /// Rackspace Chicago Datacenter
-        /// </summary>
-        public const string RACKSPACE_ORD = "mq-rackspace-ord.iron.io";
-
-        /// <summary>
-        /// Deprecated - please use ORD.
-        /// </summary>
-        public const string RACKSPACE_DFW = "mq-rackspace-dfw.iron.io";
+        public const string AWS_US_EAST_HOST = "mq-aws-us-east-1-1.iron.io";
     }
 }
